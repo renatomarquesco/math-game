62e1e7ea1145f4265a6119b40f01d96cee4445cc
renatomarquesco <67842526+renatomarquesco@users.noreply.github.com>
2020-08-18 17:39:19 +0200

    Add files via upload

--- math.js
+++ math.js
@@ -11,6 +11,8 @@ var range = document.getElementById("numRange");
 var label = document.getElementById("label");
 var valueRange = 10;
 var firstRow = document.getElementById("first-row")
+var range = document.getElementById("numRange");
+var label = document.getElementById("label");
 
 // Label range
 
@@ -23,7 +25,6 @@ range.oninput = function () {
 // generatenumbers
 
 var generateNumbers = function () {
-    console.log(valueRange)
     var number1 = Math.floor(Math.random() * valueRange);
     var number2 = Math.floor(Math.random() * valueRange);
 
@@ -33,12 +34,13 @@ var generateNumbers = function () {
     if(number2===0){
         number2=10;
     }
-
+    firstRow.style.visibility = "visible"
     firstNumber.innerHTML = number1;
     secondNumber.innerHTML = number2;
     operationSign.innerHTML= "+"
     randomNumbers.push(number1);
-    randomNumbers.push(number2);   
+    randomNumbers.push(number2);
+  
 }
 
 // function to checkAnswer
@@ -60,13 +62,16 @@ function showTimeLeft() {
 
     timeLeft.innerHTML = i + " seconds"
     var value = Number(input.value);
+
     i--;
 
     if (i < 0) {
         firstRow.setAttribute("class", "text-center time-is-up")
         firstRow.innerHTML = ("<img src=./imgs/contagem.png>") + "Time is up!"
+        firstRow.style.color = "yellow"
         document.getElementById("start-btn").style.display="none"
         document.getElementById("try-again-btn").style.display="inline"
+
 
         clearInterval(interval);
     }
@@ -88,11 +93,13 @@ document.getElementById("start-btn").onclick = function () {
     generateNumbers();
     interval = setInterval("showTimeLeft()", 1000)
     this.disabled=true;
+    document.getElementById("div-range").style.display="none";
 }
+
 
 document.getElementById("try-again-btn").onclick = function () {
     window.location.reload();
+   
 }
 
-var range = document.getElementById("numRange");
-var label = document.getElementById("label");
+
